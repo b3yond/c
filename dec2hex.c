4c3be0c53f99dfda7a4c72e11d6fa6e0cd47a1ec
b3yond <b3yond@riseup.net>
2016-12-21 14:10:26 +0100

    changed filenames

---
+++ dec2hex.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int zahl;
+    printf("Welche Zahl möchtest du konvertieren: ");
+    scanf("%d",&zahl);
+    printf("Der Hexcode dazu lautet: %X\n",zahl);
+return 0;
+}
---
+++ hex2dec.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int zahl;
+    printf("Welchen Hexcode möchtest du konvertieren: ");
+    scanf("%x",&zahl);
+    printf("Der Hexcode dazu lautet: %d\n",zahl);
+return 0;
+}
